ba78ea7a569a8a5e49e840e9dd3f16172ea024e6
Aaron Lee Marais <aaronleem@gmail.com>
2019-09-09 09:35:44 +0200

    Complete task 15

--- src/server.js
+++ src/server.js
@@ -1,18 +1,30 @@
+/*
+	This file holds my server info
+*/
+
+// Im;port JSON5 library
 require('json5/lib/register')
+
+// Import person
 const person = require("./person.json5")
+
+// Import express and create app
 const express = require("express")
 const app = express()
 
+// Use public dir for static files
 app.use(express.static("public"))
 
+// Root path should welcome user
 app.get("/", (rq, rs) => {
-    rs.send("welcome")
+    rs.send(`Welcome, ${person.name}`)
 })
 
-app.get('*', (rq, rs, nx) => {
-  const err = new Error(`Page Not Found: ${rq.originalUrl}`)
-  nx(err)
-});
+// Any other path except root and static will display message
+app.get('*', (rq, rs) => {
+  rs.send("Sorry, can't find that resource!")
+})
 
+// Listen on port 3000
 app.listen(3000)
 
